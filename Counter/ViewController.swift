da35dd811d7f3d0bddd077c34d9bcab692d612dc
stpkch <kachusovstepa@gmail.com>
2025-05-26 17:36:20 +0500

    Задание выполнено

---
+++ Counter/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Качусов Степан on 26.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    private var counter = 0
+    
+    @IBOutlet weak var encounter: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func tapButton(_ sender: Any) {
+        counter += 1
+        encounter.text = "Значение счётчика: \(counter)"
+    }
+    
+}
+
---
+++ CounterTests/CounterTests.swift
@@ -0,0 +1,17 @@
+//
+//  CounterTests.swift
+//  CounterTests
+//
+//  Created by Качусов Степан on 26.05.2025.
+//
+
+import Testing
+@testable import Counter
+
+struct CounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
